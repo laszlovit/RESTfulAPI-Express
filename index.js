049bafbc3ee31b0112d1a3f5989a08046d4473a9
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2024-02-18 18:26:42 +0100

    added the necessary dependencies (express, mongoose, dotenv) and established the db connection

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+require("dotenv").config();
+
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
