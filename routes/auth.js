e7fbe2c96e45bc4003b169e6e3a0fa2623dc49bd
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2024-02-26 10:44:04 +0100

    completed login route

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 const User = require("../models/user");
-const { registerValidation } = require("../validation");
+const { registerValidation, loginValidation } = require("../validation");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = router;
 
@@ -45,5 +46,44 @@ router.post("/register", async (req, res) => {
 
 // /login
 router.post("/login", async (req, res) => {
-  return res.status(200).json({ message: "Login route" });
+  //validate the user input (email, password)
+  const { error } = loginValidation(req.body);
+
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+
+  // if login info is valid, find the user
+  const user = await User.findOne({ email: req.body.email });
+
+  // throw error if email is wrong (user does not exist in DB)
+  if (!user) {
+    return res.status(400).json({ error: "Email is wrong" });
+  }
+
+  // user exists - check for password correctness
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+  //throw error if password is wrong
+  if (!validPassword) {
+    return res.status(400).json({ message: "Password is wrong" });
+  }
+
+  //create auth token with username and id
+  const token = jwt.sign(
+    //payload
+    {
+      name: user.name,
+      id: user._id,
+    },
+    //secret key
+    process.env.TOKEN_SECRET,
+    //exipration time
+    { expiresIn: process.env.JWT_EXPIRES_IN }
+  );
+  //attach auth token to the header
+  res.header("auth-token", token).json({
+    error: null,
+    data: { token },
+  });
 });
--- validation.js
+++ validation.js
@@ -11,7 +11,14 @@ const registerValidation = (data) => {
 };
 
 // validating login
+const loginValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(8).max(255).required(),
+  });
+  return schema.validate(data);
+};
 
 // logic to verify our token (JWT)
 
-module.exports = { registerValidation };
+module.exports = { registerValidation, loginValidation };
