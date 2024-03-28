ea30998a324d3b6226299345572933dec78733ee
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2024-03-28 12:11:16 +0100

    added render.com cors

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://res-tful-api-node-express-mongo-db.vercel.app/",
+      "https://restfulapi-node-express-mongodb.onrender.com",
     ],
   })
 );
